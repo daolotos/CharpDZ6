b524b50bacf649a57f262a96ab43a2f0c186999d
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-15 09:21:36 +0300

    Initial commit

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int N = ReadInt("Введите размер массива: ");
+int[] array = new int[N];
+
+
+Console.WriteLine("Количество четных чисел: ");
+
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
