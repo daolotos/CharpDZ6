d39acbd3651cfd6a60d6a1c2cf04c865bcac8d7f
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-15 09:35:33 +0300

    Сделала pflfxe 43

---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Рассчет пересечения 2х прямых y = k1*x + b1 и y = k2*x + b2");
+float k1 = ReadFloat("k1 = ");
+float b1 = ReadFloat("b1 = ");
+float k2 = ReadFloat("k2 = ");
+float b2 = ReadFloat("b2 = ");
+
+if (k1 != k2)
+{
+    float x = (b2 - b1) / (k1 - k2);
+    float y = k1 * x + b1;
+
+    Console.WriteLine($"Прямые пересекаются в ({x},{y})");
+}
+else
+    Console.WriteLine("Прямые не пересекаются");
+
+float ReadFloat(string prompt)
+{
+    Console.Write(prompt);
+    float x = (float)Convert.ToDouble(Console.ReadLine());
+    return x;
+}
